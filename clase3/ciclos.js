35b1b3aeb82f2572ea68fe6c02ccc0d27d71a307
cesmar <espinomaravic@gmail.com>
2014-10-12 19:23:26 -0500

    Clase 3
    Primero archivos de la clase 3

---
+++ clase3/ciclos.js
@@ -0,0 +1,23 @@
+var c,t,b;
+
+function inicio()
+{
+	c = document.getElementById("caja");
+	t = document.getElementById("alejandra");
+	b = document.getElementById("botoncito");
+
+	b.addEventListener("click", calcular);
+}
+
+function calcular () 
+{
+	var numero = Number(t.value);
+	var calculo = 1;
+	var n;
+
+	for(n=1; n<=numero; n++)
+	{
+		calculo = calculo * n;
+	}
+	c.innerHTML = "<strong>" + calculo + "</strong>";
+}
